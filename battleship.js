42321ca92f951377ee5ee0e2c0fddb16a7cf6a81
Densel Martin <denselmartin@gmail.com>
2016-09-06 14:37:11 -0700

    hit button
    tried to  add hit

--- battleship.js
+++ battleship.js
@@ -21,6 +21,8 @@ var letterConversion = {
 	"J": 9
 }
 
+var letterArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+
 // makes the grid columns and rows
 for (i = 0; i < cols; i++) {
 	for (j = 0; j < rows; j++) {
@@ -34,6 +36,8 @@ for (i = 0; i < cols; i++) {
 		square.className = "boardSquare";
 
 		// THIS IS WHERE YOU WILL ADD CODE FOR PART 1 TO ADD TEXT TO EACH SQUARE
+
+		square.textContent = letterArray[j] + (i + 1);
 
 		// set each grid square's coordinates: multiples of the current row or column number
 		var topPosition = j * squareSize;
@@ -60,6 +64,10 @@ var gameBoard = [
 				]
 
 function fireTorpedo() {
+
+userInput = document.getElementById("textInput").value;
+rowLetter = userInput.substring(0,1);
+console.log(rowLetter);
 
 	// Your game logic will go here!
 
